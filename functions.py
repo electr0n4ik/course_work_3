1c35e328914470a7330df9a2582292e0b9373878
electr0n4ik <andreyshka3@gmail.com>
2023-04-25 11:22:23 +0300

    upd file:
    functions.py
    
    add # 2 func

--- functions.py
+++ functions.py
@@ -6,6 +6,18 @@ def load_operations():
     """
     import json
 
-    with open("candidates.json", encoding="utf-8") as file:
+    with open("operations.json", encoding="utf-8") as file:
         operations = json.load(file)
     return operations
+
+
+# 2
+def get_5_last_operations(list_operations):
+    """
+    Получает список операций и возвращает 5 последних выполненных операций
+    :param list_operations: список операций
+    :return: 5 последних выполненных операций
+    """
+
+    for operation in list_operations:
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from functions import *
+
+for _ in load_operations():
+    print(_)
